5ab3b8333740411a0ab4c864d1f19266ca71e462
Weinan <weli@redhat.com>
2016-08-29 01:20:25 +0800

    add 07

---
+++ 07/src/main/java/org/weli/gradle/Foo.java
@@ -0,0 +1,12 @@
+package org.weli.gradle;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by weli on 8/28/16.
+ */
+public class Foo {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("hello, world!"));
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+#  『阿男的Gradle学习笔记』
+
+## 视频专栏：http://list.youku.com/albumlist/show?id=27464364
+## 教案：https://www.douban.com/note/tags/gradle?people=weinanli
